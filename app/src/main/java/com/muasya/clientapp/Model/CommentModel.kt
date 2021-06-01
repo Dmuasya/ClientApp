db5974c496b45bff6c84f69f442fb71a2b31082a
DMuasya <muasyadennis4@gmail.com>
2021-06-01 11:42:21 +0300

    Rating food

---
+++ app/src/main/java/com/muasya/clientapp/Model/CommentModel.kt
@@ -0,0 +1,9 @@
+package com.muasya.clientapp.Model
+
+class CommentModel {
+    var ratingValue:Float=0.toFloat()
+    var comment:String?=null
+    var name:String?=null
+    var uid:String?=null
+    var commentTimestamp:Map<String,Any>?=null
+}
